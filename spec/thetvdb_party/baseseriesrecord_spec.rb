b8952af4a29e490e6510e12aff62e05acfa94723
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2015-02-23 22:48:57 +0100

    Fixed Base Series Record Spec: id renamed to seriesid

--- spec/thetvdb_party/baseseriesrecord_spec.rb
+++ spec/thetvdb_party/baseseriesrecord_spec.rb
@@ -12,13 +12,10 @@ describe 'BaseSeriesRecord' do
   end
 
   it "should have valid id if valid" do
-    expect @valid_record.id == 0
+    expect @valid_record.seriesid == 0
   end
 
   it "should have negative id if series not found or invalid" do
-    expect @invalid_record.id < 0
+    expect @invalid_record.seriesid < 0
   end
-
-  it "must have a hash to be created" do
-    expect(TheTvDbParty::BaseSeriesRecord.new(nil, nil)).to raise_error
 end
