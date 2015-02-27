6ea49a1e013e86d6f42f7c8722d11f62b176896f
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2015-02-27 20:30:20 +0100

    Fixed bug in Search Series Record -> Hash values are now read the right way

--- lib/thetvdb_party/searchseriesrecord.rb
+++ lib/thetvdb_party/searchseriesrecord.rb
@@ -54,19 +54,18 @@ module TheTvDbParty
 
     private
     def read_hash_values
-      def client; @client end
-      def id; @hashValues["id"] ? @hashValues["id"].to_i : -1 end
-      def seriesid; @hashValues["seriesid"] ? @hashValues["seriesid"].to_i : -1 end
-      def language; @hashValues["language"] end
-      def seriesname; @hashValues["SeriesName"] end
-      def aliasnames; @hashValues["AliasNames"] ? @hashValues["AliasNames"].split('|').reject { |a| a.nil? || a.empty? } : [] end
-      def bannerpath_relative; @hashValues["banner"] end
-      def overview; @hashValues["Overview"] end
-      def firstaired; @hashValues["FirstAired"] ? Date.parse(@hashValues["FirstAired"]) : nil end
-      def imdb_id; @hashValues["IMDB_ID"] end
-      def zap2it_id; @hashValues["zap2it_id"] end
-      def network; @hashValues["Network"] end
-      def bannerpath_full; bannerpath_relative ? URI::join(BASE_URL, "banners/", bannerpath_relative) : nil end
+      @id = @hashValues["id"] ? @hashValues["id"].to_i : -1
+      @seriesid = @hashValues["seriesid"] ? @hashValues["seriesid"].to_i : -1
+      @language = @hashValues["language"]
+      @seriesname = @hashValues["SeriesName"]
+      @aliasnames = @hashValues["AliasNames"] ? @hashValues["AliasNames"].split('|').reject { |a| a.nil? || a.empty? } : []
+      @bannerpath_relative = @hashValues["banner"]
+      @overview = @hashValues["Overview"]
+      @firstaired = @hashValues["FirstAired"] ? Date.parse(@hashValues["FirstAired"]) : nil
+      @imdb_id = @hashValues["IMDB_ID"]
+      @zap2it_id = @hashValues["zap2it_id"]
+      @network = @hashValues["Network"]
+      @bannerpath_full = bannerpath_relative ? URI::join(BASE_URL, "banners/", bannerpath_relative) : nil
 
       nil
     end
